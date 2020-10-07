0bd28450c39d9f19e0b06ec9ba4d5ae2a89ed4a8
NotZombieFood <gerardocd1000@gmail.com>
2020-10-07 04:27:11 +0000

    practica 5

---
+++ course_work/practica5/suma.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(){
+    char c;
+    unsigned i = 0;
+    char buffer[250];
+    unsigned total = 0;
+    while(read(STDIN_FILENO,&c,1) != 0){
+        if(c != ' '){
+            buffer[i++] = c;
+        }else{
+            buffer[i] = '\0';
+            total += atoi(buffer);
+            i = 0;
+        }
+    }
+    buffer[i] = '\0';
+    total += atoi(buffer);
+    printf("%u\n", total);
+} 
